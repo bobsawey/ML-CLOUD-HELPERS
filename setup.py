091d22529ff608658c381f0725112d9ad2f82984
Bobby Jo <bobsawey@gmail.com>
2018-08-01 01:27:38 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='ko',   
+    version='0',                          # Update the version number for new releases
+    scripts=['ko']                  # The name of your scipt, and also the command you'll be using for calling it
+)
